433433dcb07227aa157d2397215c7a6c3eb419e9
carly <carly@noukod.com>
2018-09-20 17:04:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,10 @@
 # Write your code here.
-def take_a_number
-  if line false
-    array << 1_person
-  elsif line true
-    array.shift
-  else
-    array
-
-end
-end
 
 def now_serving
   if line false
     puts "the line is empty"
   else
-end    
+end
 end
 
 def line katz_deli
